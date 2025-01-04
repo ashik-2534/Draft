00d8fb92cadb79e45033ea3d5096e952d1296039
Ashik_Ridwan <ridwanashik004@gmail.com>
2025-01-04 21:22:30 +0600

    added & modified

--- Javascript/let's see 1.js
+++ Javascript/let's see 1.js
@@ -39,3 +39,42 @@ function increment(){
     console.log(count)
 }
 increment()
+
+````
+let username = "per"
+
+// Create a variable, message, that stores the string: "You have tree new notifications"
+let message = "You have tree new notifications"
+
+console.log(meaasge + ", " + username + "!")
+
+// Create a variable, messageToUser, that contains the message we have logged
+let messageToUser = meaasge + ", " + username + "!"
+
+console.log(messageToUser)
+
+````
+
+// Create two variables, name and greeting. The name variable should store your name,
+// and the greeting should store e.g. "Hi, my name is "
+
+// Create a third variable, myGreeting, that contatenates the two strings
+// Log myGreeting to the console
+
+
+let name = "ASHIK"
+let greeting = "Hi, my name is "
+let myGreeting = greeting + name
+
+console.log(myGreeting)
+
+
+````
+
+console.log(4 + 5) //9
+console.log("2" + "4") //"24"
+console.log("5" + 1) //"51"
+console.log(100 + "100") //"100100"
+
+````
+
