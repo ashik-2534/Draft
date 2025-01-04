4dd0f980637c84519cb26d2c6d161d29af848f59
Ashik_Ridwan <ridwanashik004@gmail.com>
2025-01-04 15:51:20 +0600

    count added

--- Javascript/let's see 1.js
+++ Javascript/let's see 1.js
@@ -27,3 +27,15 @@ lapNum()
 lapNum()
 
 console.log(lapsCompleted)
+
+
+````
+// intialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked (log it out)
+let count = 0
+function increment(){
+    count = count + 1
+    console.log(count)
+}
+increment()
