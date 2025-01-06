31c0005456cd81cec9f466e806eec929ec742bb0
Ashik_Ridwan <ridwanashik004@gmail.com>
2025-01-06 12:12:17 +0600

    modified

--- Javascript/lets see2 .js
+++ Javascript/lets see2 .js
@@ -1,5 +1,10 @@
 // Create two variables, firstName and lastName
+let firstName = "Ashik "
+let lastName = "Ridwan"
 
 // Concatenate the two variables into a third variable called fullName
+let fullName = firstName + lastName
 
 // Log fullName to the console
+
+console.log(fullName)
