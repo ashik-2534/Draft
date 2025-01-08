60eb3749b097deaccd93891d6c634206d75da9a0
Ashik_Ridwan <ridwanashik004@gmail.com>
2025-01-08 20:09:31 +0600

    Day 2 added in react

---
+++ Javascript/React/Day 2/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div>
+      <div className="p-5 bg-gradient-to-tr from-blue-200 via-white to-blue-400 text-center">
+        <h1 className="text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl ">Hello World!</h1>
+        <button>Dark Mode</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
