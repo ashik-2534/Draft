8de05464a5ec9d04d047f21ee1205806b5398698
Ashik_Ridwan <ridwanashik004@gmail.com>
2025-02-15 18:23:15 +0600

    created urls.py and added views , urls

--- Django/Day 4/PracticeProject/demoApp/views.py
+++ Django/Day 4/PracticeProject/demoApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def home(hm):
+    return HttpResponse ("homepage")
+def ashik(ask):
+    return HttpResponse ("hello ashik")
+
+def 
