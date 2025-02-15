54085603445ed764ed1690f620b1793a657a6a6a
Ashik_Ridwan <ridwanashik004@gmail.com>
2025-02-15 20:36:21 +0600

    created index.html templates for newyear app

--- Django/Day 4/PracticeProject/newyear/views.py
+++ Django/Day 4/PracticeProject/newyear/views.py
@@ -1,6 +1,10 @@
+import datetime
 from django.shortcuts import render
 
 # Create your views here.
 
-# def index(request):
-#     return render (request, "newyear/index.html")
+def index(request):
+    now = datetime.datetime.now()
+    return render (request, "newyear/index.html",{
+        "theDate": now.month == 1 and now.day == 1,
+    })
