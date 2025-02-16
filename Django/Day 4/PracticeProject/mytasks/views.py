02c8521c167a7efc63a289ec9114486d0f415d19
Ashik_Ridwan <ridwanashik004@gmail.com>
2025-02-16 09:59:44 +0600

    completed class

--- Django/Day 4/PracticeProject/mytasks/views.py
+++ Django/Day 4/PracticeProject/mytasks/views.py
@@ -1,10 +1,32 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from django import forms
 
-ajaitakaj = ["A task" , "B task" , "C task"]
+
+class KajJogkoro(forms.Form):
+    kajerNam = forms.CharField(label = " kaj ki ")
+
+
 def index(request):
+    if "ajairakaj" not in request.session:
+        request.session["ajairakaj"] = []
     return render (request, "mytask/index.html",{
-        "tasks": ajaitakaj,
+        "tasks": request.session["ajairakaj"],
     })
 
 def add (request):
-    return render (request, "mytask/addtask.html")
+    if request.method == "POST":
+        khata = KajJogkoro(request.POST)
+        if khata.is_valid():
+            likhakaj = khata.cleaned_data["kajerNam"]
+            request.session["ajairakaj"] += [likhakaj]
+            return HttpResponseRedirect (reverse("mytasks:index"))
+        else:
+            return render(request, "mytask/addtask.html",{
+                "ajairakhata": khata
+            })
+        
+    return render (request, "mytask/addtask.html",{
+        "juktokaj" : KajJogkoro(),
+    })
