46cc6a631c533b08b0cfa7ac3b169fe14066b016
Ashik_Ridwan <ridwanashik004@gmail.com>
2025-02-18 11:19:28 +0600

    modified models

--- Django/Day 5/Practice/demoApp/models.py
+++ Django/Day 5/Practice/demoApp/models.py
@@ -4,3 +4,5 @@ from django.db import models
 class person(models.Model):
     firstName = models.CharField(max_length=10)
     lastName = models.CharField(max_length=10)
+    phone = models.IntegerField(max_length=11, null=True)
+    date = models.DateField(null=True)
