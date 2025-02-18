0729677fc29e5bdb73d365c2d962995f9c20636e
Ashik_Ridwan <ridwanashik004@gmail.com>
2025-02-18 11:25:20 +0600

    added datedetail url in main project

--- Django/Day 5/Practice/myapp/urls.py
+++ Django/Day 5/Practice/myapp/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('demoApp.urls')),
+    path('datetime/', include('datedetail.urls')),
 ]
