255e4e992a5de57873fcb96b81efab4674952628
Ashik_Ridwan <ridwanashik004@gmail.com>
2025-02-19 10:32:18 +0600

    8. Created model for Job and Application 9. Migrated the models and created markdown

---
+++ Django/Day 6/README.md
@@ -0,0 +1,11 @@
+# Steps breakdown
+
+1. Created an virtual environment named "myWnv" ```python -m venv myWnv```
+2. Activated virtual environment "myWnv\Scripts\activate"
+3. Installed Django ```pip install django```
+4. Created a django project named "jobportal" ```python manage.py startproject jobportal```
+5. Got into the project ```cd jobportal```
+6. Created a new app named "Postjob" ```python manage.py startapp Postjob```
+7. Registered your app to settings
+8. Created model for Job and Application
+9. Migrated the models
--- Django/Day 6/jobportal/Postjob/models.py
+++ Django/Day 6/jobportal/Postjob/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Job(models.Model):
+    title = models.CharField(max_length=255)
+    company = models.CharField(max_length=255)
+    description = models.TextField()
+    location = models.CharField(max_length=255)
+    posted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+class Application(models.Model):
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    resume = models.FileField(upload_to="resume/")
+    cover_letter = models.TextField()
+    appllied_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} applied for {self.job.title}"
