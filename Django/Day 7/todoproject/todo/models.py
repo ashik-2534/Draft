755e7c96c1d75fd0bac26827ee662c09fe7ea439
Ashik_Ridwan <ridwanashik004@gmail.com>
2025-04-14 23:32:12 +0600

    added more fields in model

--- Django/Day 7/todoproject/todo/models.py
+++ Django/Day 7/todoproject/todo/models.py
@@ -1,8 +1,18 @@
+from email.policy import default
 from django.db import models
 
 class Task(models.Model):
-    title = models.CharField(max_length=200)
-    completed = models.BooleanField(default=True)
+    PRIORITY_CHOICES = [
+        ('LOW'),
+        ('MEDIUM'),
+        ('HIGH')
+    ]
 
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    completed = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    priority = models.CharField(max_length=6, choices= PRIORITY_CHOICES, default= 'MEDIUM')
     def __str__(self):
         return self.title
