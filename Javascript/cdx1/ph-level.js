773188f3d0aac58500810ba856d1bef0bda53b0e
Ashik_Ridwan <ridwanashik004@gmail.com>
2025-04-16 21:47:41 +0600

    created ph level file

---
+++ Javascript/cdx1/ph-level.js
@@ -0,0 +1,21 @@
+// In chemistry, pH is a scale used to specify the acidity or basicity of a fluid.
+
+// Create a ph-levels.js program that checks whether a pH level is basic, acidic, or neutral.
+
+// First, create a variable called ph and give it a value between 0 and 14.
+
+// Write an if/else if/else statement that:
+
+// If ph is greater than 7, output “Basic”.
+// Else if ph is less than 7, output “Acidic”.
+// Else, output “Neutral”.
+
+let ph = 7
+
+if (ph > 7){
+    console.log("Basic. Likely soap.")
+}else if(ph < 7){
+    console.log("Acdic. Likely acid.")
+}else{
+    console.log("Neutral. Like water.")
+}
