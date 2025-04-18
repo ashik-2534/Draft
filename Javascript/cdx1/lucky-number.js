d61fe36e119e24cb0ba6616eea68daf78ff5e2bc
Ashik_Ridwan <ridwanashik004@gmail.com>
2025-04-18 21:04:56 +0600

    understood the lucky numer code

---
+++ Javascript/cdx1/lucky-number.js
@@ -0,0 +1,11 @@
+// lucky-number.js
+
+const luckyNumber = 7;
+let guess = Math.floor(Math.random() * 10) + 1;
+
+while (guess !== luckyNumber) {
+  console.log(`Nope, it isn't ${guess}`);
+  guess = Math.floor(Math.random() * 10) + 1;
+}
+
+console.log(luckyNumber);
